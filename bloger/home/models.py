e8318dac151eaab072693437e07c4956891e0ec6
duvindu-embuldeniya <duvindu.personal@gmail.com>
2025-04-19 17:57:06 +0530

    +profile model

--- bloger/home/models.py
+++ bloger/home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE) 
+    image = models.ImageField(upload_to='profile_model/', null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+    
+    @property
+    def img_url(self):
+        try:
+            return self.image.url
+
+        except Exception as ex:
+            return '/static/images/static_profile_model/default.png'
