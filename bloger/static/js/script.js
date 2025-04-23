c99010a504f3ed5ec1508858ae80f545e2cc0bc6
duvindu-embuldeniya <duvindu.personal@gmail.com>
2025-04-23 09:32:29 +0530

    add js

--- bloger/static/js/script.js
+++ bloger/static/js/script.js
@@ -1,1 +1,16 @@
 // alert("hellow world!");
+
+let form = document.getElementById('form')
+let pages = document.getElementsByClassName('page')
+
+for(let i=0; i<pages.length; i++){
+    pages[i].addEventListener('click', function(e){
+        e.preventDefault()
+
+        let page = this.dataset.page
+
+        form.innerHTML += `<input type='text' name='page' value=${page}>`
+        
+        form.submit();
+    })
+}
